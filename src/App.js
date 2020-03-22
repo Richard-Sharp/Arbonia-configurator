d4798046bdbd906a335e1e57e10196b427f5e258
Rachard-Sharp <novitskii.a@gmail.com>
2020-03-22 20:01:05 +0300

    Init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+			<div className="App">
+				<h2>Arbonia configurator </h2>
+				<h5>Ver: 1.0.1</h5>
+
+			</div>
+	);
+}
+
+export default App;
