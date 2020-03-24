4a996c0e86894f36e7517af1c1f3094ce3587835
Rachard-Sharp <novitskii.a@gmail.com>
2020-03-24 22:28:13 +0300

    Ver: 1.0.1_7: Added Route-NavLink switching. Added componets RRN and RRV.

--- src/components/ExtraOptions/ExtraOptions.js
+++ src/components/ExtraOptions/ExtraOptions.js
@@ -2,15 +2,26 @@ import React from 'react';
 import style from './ExtraOptions.module.css';
 import ArboniaRRN from "./RRN/ArboniaRRN";
 import ArboniaRRV from "./RRV/ArboniaRRV";
+import {Route} from "react-router";
+import {NavLink} from "react-router-dom";
 
 
 //Компонент для настройки дополнительных параметров прибора:
 const ExtraOptions = (props) => {
 	return <div className={style.container}>
 		<h3>Исполнение (наличие встроенного вентиля):</h3>
+		<div className={style.sideBar}>
+			<span className={style.button}>
+          <NavLink to="/withoutVentil" activeClassName={style.active}>RRN</NavLink></span>
+			<span className={style.button}>
+					<NavLink to="/withVentil" activeClassName={style.active}>RRV</NavLink>
+        </span>
+		</div>
+
 		<div>
-			<ArboniaRRN />
-			<ArboniaRRV />
+			<Route path='/withoutVentil' render={() => <ArboniaRRN/>}/>
+			<Route path='/withVentil' render={() => <ArboniaRRV/>}/>
+
 		</div>
 
 	</div>
