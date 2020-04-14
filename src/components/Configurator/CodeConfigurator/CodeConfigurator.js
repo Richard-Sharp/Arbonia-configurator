50c22bf6b45ca2cf0446f7c9bc1a8fbac5bfa689
Rachard-Sharp <novitskii.a@gmail.com>
2020-04-14 22:21:34 +0300

    Ver: 1.0.1_25: Field with the name is added.

--- src/components/Configurator/CodeConfigurator/CodeConfigurator.js
+++ src/components/Configurator/CodeConfigurator/CodeConfigurator.js
@@ -4,12 +4,22 @@ import style from './CodeConfigurator.module.css';
 //Компонент для отображения артикула прибора:
 const CodeConfigurator = ({arboniaModel, quantitySections, valve, connectionType, connectionSize, airCooler, airCoolerLocation, airCoolerSize, drain }) => {
 
-	let article = `${valve.model} ${arboniaModel}${valve.model === 'RRV' ? 'V' : ''} ${quantitySections}/${valve.code}/${connectionType.code}/${connectionSize.code}/${airCooler.code}/${airCoolerLocation.code}/${airCoolerSize.code}/${drain.code} AF`
+	let article = `${valve.model} ${arboniaModel}${valve.model === 'RRV' ? 'V' : ''} ${quantitySections}/${valve.code}/${connectionType.code}/${connectionSize.code}/${airCooler.code}/${airCoolerLocation.code}/${airCoolerSize.code}/${drain.code} AF`;
+
+	let name = `Радиатор Arbonia ${arboniaModel}${valve.model === 'RRV' ? 'V' : ''} ${quantitySections}/${valve.code}/${connectionType.code}/${connectionSize.code}/${airCooler.code}/${airCoolerLocation.code}/${airCoolerSize.code}/${drain.code} AF`;
 
 	return <div className={style.container} >
 		<h3>Артикул прибора:</h3>
+		<div>
+			<strong>Артикул: </strong>
+			<input type="text" value={article}/>
+		</div>
+		<div>
+			<strong>Наименование: </strong><input type="text" value={name}/>
+		</div>
 
-		<input type="text" value={article}/>
+
+
 
 
 
