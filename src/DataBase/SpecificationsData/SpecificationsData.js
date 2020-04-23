56b564a3d2685df9b260adec65d3a74768a2f1f6
Rachard-Sharp <novitskii.a@gmail.com>
2020-04-23 21:41:17 +0300

    Ver: 1.0.2_1: Filling the database.

--- src/DataBase/SpecificationsData/SpecificationsData.js
+++ src/DataBase/SpecificationsData/SpecificationsData.js
@@ -29,16 +29,131 @@ export const getArboniaModelData = (value) => (
 		}[value] || arb2030);
 
 //(модель, высота, трубки, глубина, мощность, цена)
-const arb2030 = arboniaModel(2030, 24, 13.70);
-const arb3030 = arboniaModel(3030, 33, 14.86);
-const arb4030 = arboniaModel(4030, 43, 16.44);
-const arb5030 = arboniaModel(5030, 53, 19.65);
-const arb6030 = arboniaModel(6030, 53, 22.95);
-
-const arb2050 = arboniaModel(2050, 49, 14.84);
-const arb3050 = arboniaModel(3050, 53, 16.72);
-const arb4050 = arboniaModel(4050, 69, 18.61);
-const arb5050 = arboniaModel(5050, 85, 22.01);
-const arb6050 = arboniaModel(6050, 99, 25.89);
-
+//180
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//190
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+//260
+const arb2026 = arboniaModel(2026, 24, 14.63);
+const arb3026 = arboniaModel(3026, 33, 15.88);
+const arb4026 = arboniaModel(4026, 43, 17.57);
+const arb5026 = arboniaModel(5026, 53, 21.00);
+const arb6026 = arboniaModel(6026, 53, 24.52);
+//300
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//350
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//400
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//450
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//500
+const arb2050 = arboniaModel(2050, 49, 15.86);
+const arb3050 = arboniaModel(3050, 53, 17.86);
+const arb4050 = arboniaModel(4050, 69, 19.89);
+const arb5050 = arboniaModel(5050, 85, 23.52);
+const arb6050 = arboniaModel(6050, 99, 27.66);
+//550
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//600
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//750
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//900
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//1000
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//1100
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//1200
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//1500
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//1800
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//2000
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//2200
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//2500
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//2800
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
+//3000
+const arb2030 = arboniaModel(2030, 24, 14.63);
+const arb3030 = arboniaModel(3030, 33, 15.88);
+const arb4030 = arboniaModel(4030, 43, 17.57);
+const arb5030 = arboniaModel(5030, 53, 21.00);
+const arb6030 = arboniaModel(6030, 53, 24.52);
 
