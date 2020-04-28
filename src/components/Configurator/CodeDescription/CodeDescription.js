9e9307e80bca3b7d089771e429cb990e086be1a4
Rachard-Sharp <novitskii.a@gmail.com>
2020-04-28 21:42:04 +0300

    Ver: 1.0.2_5: Filling the database (SpecificationData).

--- src/components/Configurator/CodeDescription/CodeDescription.js
+++ src/components/Configurator/CodeDescription/CodeDescription.js
@@ -7,7 +7,6 @@ const CodeDescription = ({arboniaModel, height, type, quantitySections, valve, c
 
 	let description = `Радиатор Arbonia ${arboniaModel}${!valve.valveExistence ? '' : 'V'}  ${type.code}-трубчатый ${valve.description}, глубиной - ${type.value} мм, высотой - ${height.value} мм. Количество секций - ${quantitySections}, общей длиной - ${totalLenght} мм, подключение ${connectionType.description} с диаметром ${connectionSize.description}, рабочее давление: ${pressure.description}. Общая мощность прибора при dT50 = ${totalPower} Вт.`;
 
-
 	return <div className={style.container}>
 		<h3>Описание прибора:</h3>
 		<div>
