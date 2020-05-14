72c8cb94fd2d0676a86f1944bafa3185cf9eb62a
Rachard-Sharp <novitskii.a@gmail.com>
2020-05-14 22:33:41 +0300

    Ver: 1.0.2_11: Populating the database ExtraOptionsData.

---
+++ src/DataBase/ExtraOptionsData/ExtraOptionsData.js
@@ -0,0 +1,16 @@
+// {name, data-price, data-description, value, airCoolerLocation, text}
+
+export const connectionTypeData = [
+	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое справа', value: '34', airCoolerLocation: '1', text: '14'},
+	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое слева', value: '12', airCoolerLocation: '3', text: '12'},
+	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое  диагональное справа', value: '32', airCoolerLocation: '1', text: '32'},
+	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое  диагональное слева', value: '14', airCoolerLocation: '3', text: '14'},
+
+	{name: 'connectionType', dataPrice: 11.65, dataDescription: 'нижнее проходное слева', value: '24', airCoolerLocation: '3', text: '24'},
+	{name: 'connectionType', dataPrice: 11.65, dataDescription: 'нижнее проходное справа', value: '42', airCoolerLocation: '1', text: '42'},
+
+	{name: 'connectionType', dataPrice: 28.05, dataDescription: 'проходное слева', value: '13', airCoolerLocation: '0', text: '13'},
+	{name: 'connectionType', dataPrice: 28.05, dataDescription: 'проходное справа', value: '31', airCoolerLocation: '0', text: '31'},
+	{name: 'connectionType', dataPrice: 28.05, dataDescription: 'нижнее диагональное справа', value: '41', airCoolerLocation: '3', text: '41'},
+	{name: 'connectionType', dataPrice: 28.05, dataDescription: 'нижнее диагональное слева', value: '23', airCoolerLocation: '1', text: '23'},
+]
