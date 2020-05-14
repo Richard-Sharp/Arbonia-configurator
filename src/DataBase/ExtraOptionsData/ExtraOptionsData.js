349150828a6330654348a0ba77f9a3461ec9eb31
Rachard-Sharp <novitskii.a@gmail.com>
2020-05-14 23:28:46 +0300

    Ver: 1.0.2_14: Populating the database ExtraOptionsData - connectionTypeRRNData.

--- src/DataBase/ExtraOptionsData/ExtraOptionsData.js
+++ src/DataBase/ExtraOptionsData/ExtraOptionsData.js
@@ -1,11 +1,11 @@
 // {name, data-price, data-description, value, airCoolerLocation, text}
 
+//Типы подключений радиаторов RRN:
 export const connectionTypeRRNData = [
 	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое слева', value: '12', airCoolerLocation: '3', text: '12'},
 	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое справа', value: '34', airCoolerLocation: '1', text: '34'},
 	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое  диагональное слева', value: '14', airCoolerLocation: '3', text: '14'},
 	{name: 'connectionType', dataPrice: '0', dataDescription: 'боковое  диагональное справа', value: '32', airCoolerLocation: '1', text: '32'},
-
 
 	{name: 'connectionType', dataPrice: 11.65, dataDescription: 'нижнее проходное слева', value: '24', airCoolerLocation: '3', text: '24'},
 	{name: 'connectionType', dataPrice: 11.65, dataDescription: 'нижнее проходное справа', value: '42', airCoolerLocation: '1', text: '42'},
@@ -19,4 +19,14 @@ export const connectionTypeRRNData = [
 	{name: 'connectionType', dataPrice: 59.65, dataDescription: 'внизу справа/слева', value: '86', airCoolerLocation: '1', text: '86'},
 	{name: 'connectionType', dataPrice: 59.65, dataDescription: 'сверху/снизу диагональное', value: '58', airCoolerLocation: '3', text: '58'},
 	{name: 'connectionType', dataPrice: 59.65, dataDescription: 'сверху/снизу диагональное', value: '76', airCoolerLocation: '1', text: '76'},
-];
+
+	{name: 'connectionType', dataPrice: 59.65, dataDescription: 'снизу слева', value: '69', airCoolerLocation: '3', text: '69'},
+	{name: 'connectionType', dataPrice: 59.65, dataDescription: 'снизу справа', value: '89', airCoolerLocation: '1', text: '89'},
+
+	{name: 'connectionType', dataPrice: 82.11, dataDescription: 'снизу/сверху диагональное', value: '67', airCoolerLocation: '1', text: '67'},
+	{name: 'connectionType', dataPrice: 82.11, dataDescription: 'снизу/сверху диагональное', value: '85', airCoolerLocation: '3', text: '85'},
+	{name: 'connectionType', dataPrice: 82.11, dataDescription: 'вверху слева/справа', value: '57', airCoolerLocation: '3', text: '57'},
+	{name: 'connectionType', dataPrice: 82.11, dataDescription: 'вверху справа/слева', value: '75', airCoolerLocation: '3', text: '75'},
+
+	{name: 'connectionType', dataPrice: 82.11, dataDescription: 'снизу по центру', value: '98', airCoolerLocation: '3', text: '98'},
+	{name: 'connectionType', dataPrice: 82.11, dataDescription: 'снизу по центру', value: '96', airCoolerLocation: '1', text: '96'}];
