a5e8273a2d17ab2f6134e20cdfba37c26bb3e4a4
Richard-Sharp <novitskii.a@gmail.com>
2021-02-08 21:55:03 +0300

    [Ver: 1.2.1_1]: Options bar visuals.

--- src/components/MainParams/Params/Params.js
+++ src/components/MainParams/Params/Params.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import style from './Params.module.css';
+import img2 from '../../../images/types/type2.png';
+import img3 from '../../../images/types/type3.png';
+import img4 from '../../../images/types/type4.png';
 
 const Params = (props) => {
 	let heightRef = React.createRef();
@@ -59,6 +62,26 @@ const Params = (props) => {
 					<input onChange={onChangeQuantitySections} ref={quantityRef} defaultValue={props.quantitySections}
 								 type="number" name="sectionQuantity" min="4" max="60" step="1"/>
 				</div>
+
+
+				<div className={style.typeContainer}>
+					<div className={style.radio_btn}>
+						<input id="radio-1" type="radio" name="radio" value="1"/>
+						<label htmlFor="radio-1"><img src={img2} alt="type-2"/></label>
+					</div>
+
+					<div className={style.radio_btn}>
+						<input id="radio-2" type="radio" name="radio" value="2"/>
+						<label htmlFor="radio-2"><img src={img3} alt="type-3"/></label>
+					</div>
+
+					<div className={style.radio_btn}>
+						<input id="radio-3" type="radio" name="radio" value="3"/>
+						<label htmlFor="radio-3"><img src={img4} alt="type-4"/></label>
+					</div>
+				</div>
+
+
 			</div>
 	);
 }
