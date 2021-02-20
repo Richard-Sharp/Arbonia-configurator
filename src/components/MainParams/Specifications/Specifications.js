7be3b4f38b40b1ce2af6dd64046396601286de76
Richard-Sharp <novitskii.a@gmail.com>
2021-02-20 21:23:03 +0300

    [Ver: 1.3.1_2]: Project design customization (Specifications).

--- src/components/MainParams/Specifications/Specifications.js
+++ src/components/MainParams/Specifications/Specifications.js
@@ -5,51 +5,61 @@ import style from './Specifications.module.css';
 const Specifications = (props) => {
 	return (
 			<div className={style.container}>
-				<h4>Технические характеристики</h4>
+				<h3>Технические характеристики</h3>
+				<div className={style.wrapper}>
 
-				<div>
-					<span>Модель прибора: </span>
-					<span>
-						{props.specifications.modelCode
-								? props.specifications.modelCode
-								: " .... "}
-					</span>
-				</div>
+					<div className={style.item}>
+						<span>Модель прибора: </span>
+						<div className={style.item_option}>
+							{props.specifications.modelCode
+									? props.specifications.modelCode
+									: " .... "}
+						</div>
+					</div>
 
-				<div>
-					<span>Высота прибора: </span>
-					<span>
-						{props.height.value} мм
-					</span>
-				</div>
+					<div className={style.item}>
+						<span>Высота прибора [H]: </span>
+						<div className={style.item_option}>
+							{props.height.value} мм
+						</div>
+					</div>
 
-				<div>
-					<span>Глубина прибора: </span>
-					<span>
-						{props.type.value} мм
-					</span>
-				</div>
+					<div className={style.item}>
+						<span>Глубина прибора [T]: </span>
+						<div className={style.item_option}>
+							{props.type.value} мм
+						</div>
+					</div>
 
-				<div>
-					<span>Количество секций: </span>
-					<span>
-						{props.quantitySections} шт.
-					</span>
-				</div>
+					<div className={style.item}>
+						<span>Глубина от центра [T1]: </span>
+						<div className={style.item_option}>
+							{props.type.value / 2} мм
+						</div>
+					</div>
 
-				<div>
-					<span>Длина прибора: </span>
-					<span>
-						{props.quantitySections > 0 ? props.totalLenght : 0} мм
-					</span>
-				</div>
+					<div className={style.item}>
+						<span>Длина прибора [L]: </span>
+						<div className={style.item_option}>
+							{props.quantitySections > 0 ? props.totalLenght : 0} мм
+						</div>
+					</div>
 
-				<div>
-					<span>Мощность прибора (dT50): </span>
-					<span>
-						{props.totalPower} Вт
-						{/*{!props.totalPower ? 0 : props.totalPower} Вт*/}
-					</span>
+					<div className={style.item}>
+						<span>Количество секций [GL]: </span>
+						<div className={style.item_option}>
+							{props.quantitySections} шт.
+						</div>
+					</div>
+
+
+					<div className={style.item_pw}>
+						<span>Мощность прибора (<b>ΔT50</b>/ ΔT60/ ΔT70): </span>
+						<div className={style.item_option_pw}>
+							<b>{props.totalPower}</b>/ {props.totalPower}/ {props.totalPower} Вт
+
+						</div>
+					</div>
 				</div>
 
 			</div>
