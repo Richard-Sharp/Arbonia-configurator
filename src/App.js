f88ee8c43268014a4aadb16c5c8b465f74b0d9de
Richard-Sharp <novitskii.a@gmail.com>
2021-03-05 23:20:40 +0300

    [Ver: 1.3.1_20]: Add new section (Footer).

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import MainParams from "./components/MainParams/MainParams";
 import ExtraOptions from "./components/ExtraOptions/ExtraOptions";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<Header />
 				<MainParams />
 				<ExtraOptions />
+				<Footer />
 			</div>
 	);
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import style from './Footer.module.css';
+import headerImg from '../../images/header-2.png';
+
+
+
+const Footer = () => {
+	return (
+			<div className = {style.container}>
+				<div className={style.wrapper}>
+					<div className={style.img_block}>
+						<img src={headerImg} alt="footer"/>
+					</div>
+					<div className={style.version_block}>
+						<p>Ver: 1.3.1 (beta)</p>
+					</div>
+
+				<div className={style.name_block}>
+					<p>© RchSharp, 2021</p>
+					<p>+375-29-1111111</p>
+
+				</div>
+				</div>
+			</div>
+	);
+};
+
+export default Footer;
